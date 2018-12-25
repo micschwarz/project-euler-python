136ca15fbd4ec72c13b09a60d782339dc1a19544
Michael Schwarz <089michael@gmail.com>
2018-12-25 16:11:40 +0100

    Fix typo

--- src/25/Problem25.py
+++ src/25/Problem25.py
@@ -2,7 +2,7 @@ import math
 import time
 
 
-def problem22(digits: int) -> int:
+def problem25(digits: int) -> int:
     exponent = digits - 1
     golden_ratio = (1 + 5 ** 0.5) / 2
 
@@ -11,7 +11,7 @@ def problem22(digits: int) -> int:
 
 if __name__ == '__main__':
     startTime = time.time()
-    solution = problem22(1000)
+    solution = problem25(1000)
     execTime = time.time() - startTime
     print('Solution: {0}'.format(solution))
     print('Exec-Time: {0}s'.format(execTime))
